d9950ba68e0b6f8a75a0327e16a80ee0601fb965
Jose V Sebastian <josevseb99@gmail.com>
2021-10-13 22:27:18 +0530

    complete challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setTitle(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {title}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          value={name}
+          placeholder="What's your name?"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
